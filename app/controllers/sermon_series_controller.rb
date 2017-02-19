00c13390a28e5b48b858f748d85f0a0468d1e1b4
Adam Williams <adam@thewilliams.ws>
2017-02-18 19:48:33 -0500

    Multi-file upload with progress bar

--- app/controllers/sermon_series_controller.rb
+++ app/controllers/sermon_series_controller.rb
@@ -1,4 +1,11 @@
 class SermonSeriesController < ApplicationController
   def new
   end
+
+  # Receives sermon audio files
+  def upload
+    params[:sermon_audios].each do |audio|
+      logger.info(audio.original_filename)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sermon_series/new'
+  post 'sermon_series/upload'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
